4fbca8d630b19b2f0ae7c9bd62c5974bd8d9aa12
hietpasd <hietpasd@uwosh.edu>
2016-06-02 10:13:27 -0500

    added more fields to research dbs

--- polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_research_db.py
+++ polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_research_db.py
@@ -15,32 +15,42 @@ class WSView(BrowserView):
 
         if self.request.form.get('alt','') == 'jsonp':
             return self.request.form.get('callback','?') + '(' + json.dumps(self._data) + ')'
+        print self._data
         return json.dumps(self._data)
 
 
     def process(self):
         """ do main work here """
-        brains = api.content.find(portal_type='polklibrary.type.rdb.models.database', sort_on='sortable_title', sort_order='ascending')
-        for brain in brains:
-            self._data[brain.getId] = self.transform(brain)
-
-        if 'id' in self.request.form:
-            d = self._data.get(self.request.form.get('id'), None)
-            self._data = d
+        id = self.request.form.get('id', None)
+        if id:
+            brains = api.content.find(portal_type='polklibrary.type.rdb.models.database', id=id, sort_on='sortable_title', sort_order='ascending')
+            if brains:
+                brain = brains[0]
+                self._data = self.transform(brain)
+                obj = brain.getObject()
+                self._data['message'] = obj.message.output
         else:
+            brains = api.content.find(portal_type='polklibrary.type.rdb.models.database', sort_on='sortable_title', sort_order='ascending')
+            for brain in brains:
+                self._data[brain.getId] = self.transform(brain)
             self._data = sorted(list(self._data.values()), key=lambda k: k['Title'])
-
+            
+           
     def transform(self, brain):
-        return {
+        result = {
             'id':brain.getId,
             'getId':brain.getId,
             'Title':brain.Title,
             'Description':brain.Description,
             'getURL':brain.getURL(),
             'getRemoteUrl':brain.getRemoteUrl,
+            'message': '',
+            'location': '',
             'resources':brain.resources,
         }
-
+        if brain.location:
+            result['tutorial'] = brain.location
+        return result
 
     @property
     def portal(self):
