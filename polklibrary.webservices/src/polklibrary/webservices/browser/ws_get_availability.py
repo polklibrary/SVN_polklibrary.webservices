5f32e35e8e1dce3ac0f841af50f13c1c13e22e8a
hietpasd <hietpasd@uwosh.edu>
2016-08-02 10:45:50 -0500

    setup computer availability scripts

--- polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_availability.py
+++ polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_availability.py
@@ -5,7 +5,7 @@ from Products.Five import BrowserView
 from sqlalchemy import create_engine
 from sqlalchemy.sql import select,update,functions,or_,and_
 
-import json,pymysql
+import json,pymysql,datetime
 
 class WSView(BrowserView):
 
@@ -13,7 +13,11 @@ class WSView(BrowserView):
     
     #@ram.cache(lambda *args: time() // (60 * 10))
     def __call__(self):
-        self._data = {}
+        self._data = {
+            "cached": str(datetime.datetime.now()),
+            "study_areas": [],
+            "locations": [],
+        }
         self.process()
         
         self.request.response.setHeader('Content-Type', 'application/json')
@@ -24,7 +28,49 @@ class WSView(BrowserView):
 
     def process(self):
         """ do main work here """
+        context = api.content.get(path='/library/ws/resources')
 
+        brains = api.content.find(context=context, portal_type='polklibrary.type.computeravailability.models.resource')
+        
+        allresources = []
+        for brain in brains:
+            allresources.append({
+                'status': int(brain.activated),
+                'name': brain.Title,
+                'type': brain.resources,
+                'location': brain.reference,
+            })
+            
+        self.add_location(filter(lambda x: x['location']=='1st North - EMC' and x['type']=='PC', allresources))
+        self.add_location(filter(lambda x: x['location']=='1st North - EMC' and x['type']=='MAC', allresources))
+        self.add_location(filter(lambda x: x['location']=='1st North - Polk Lab' and x['type']=='PC', allresources))
+        self.add_location(filter(lambda x: x['location']=='1st North - Polk Lab' and x['type']=='MAC', allresources))
+        self.add_location(filter(lambda x: x['location']=='1st South - Polk 101' and x['type']=='PC', allresources))
+        self.add_location(filter(lambda x: x['location']=='1st South - Polk 101' and x['type']=='MAC', allresources))
+            
+        self.add_location(filter(lambda x: x['location']=='2nd North - Main Stacks' and x['type']=='PC', allresources))
+        self.add_location(filter(lambda x: x['location']=='2nd South - Periodicals' and x['type']=='PC', allresources))
+        
+        self.add_location(filter(lambda x: x['location']=='3rd North - Quiet Study' and x['type']=='PC', allresources))
+        self.add_location(filter(lambda x: x['location']=='3rd South - Gov Info' and x['type']=='PC', allresources))
+        
+        
+            
+    def add_location(self, resources):
+        if resources:
+            type = resources[0]['type']
+            location = resources[0]['location']
+            
+            self._data['locations'].append({
+                'available': len(filter(lambda x: x['status']==1, resources)),
+                'unavailable': len(filter(lambda x: x['status']==0, resources)),
+                'name': location,
+                'total': len(resources),
+                'type': type,
+                'resources': resources,
+            })
+        
+        
         
     @property
     def portal(self):
--- polklibrary.webservices/src/polklibrary/webservices/browser/ws_set_availability.py
+++ polklibrary.webservices/src/polklibrary/webservices/browser/ws_set_availability.py
@@ -1,6 +1,8 @@
 from plone import api
+from plone.i18n.normalizer import idnormalizer
+from plone.protect.interfaces import IDisableCSRFProtection
 from Products.Five import BrowserView
-
+from zope.interface import alsoProvides
 import json
 
 class WSView(BrowserView):
@@ -8,7 +10,7 @@ class WSView(BrowserView):
     _data = {}
     
     def __call__(self):
-        self._data = {}
+        self._data = { 'status' : 'Error' }
         self.process()
         
         self.request.response.setHeader('Content-Type', 'application/json')
@@ -17,29 +19,36 @@ class WSView(BrowserView):
             return self.request.form.get('callback','?') + '(' + json.dumps(self._data) + ')'
         return json.dumps(self._data)
 
-        
-    def process(self):
+
+    def process(self):        
+        alsoProvides(self.request, IDisableCSRFProtection)
         """ do main work here """
         context = api.content.get(path='/library/ws/resources')
         
         computer_id = self.request.form.get('computerId','')
+        print computer_id
+        print idnormalizer.normalize(computer_id)
         available = True 
         if int(self.request.form.get('status', -1)) <= 0:
             available = False
         
-        brains = api.content.find(context=context, portal_type='resource_availability', title=computer_id)
+        brains = api.content.find(context=context, portal_type='polklibrary.type.computeravailability.models.resource', id=idnormalizer.normalize(computer_id))
         if brains:
             obj = brains[0].getObject()
-            obj.available = available
+            obj.activated = available
             obj.reindexObject()
+            self._data['status'] = 'Updated'
         else:
             obj = api.content.create(
-                        type='resource_availability',
+                        type='polklibrary.type.computeravailability.models.resource',
                         title=computer_id,
                         container=context,
                     )
-            obj.available = available
+            obj.activated = available
+            obj.resources = u"PC"  # Default since most are
+            api.content.transition(obj=obj, transition='publish')
             obj.reindexObject()
+            self._data['status'] = 'Created'
 
         
     @property
