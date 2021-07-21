97c31cc8a116338a92ba199930445efa03af310f
hietpasd <hietpasd@uwosh.edu>
2021-07-21 10:17:32 -0500

    fixed getComputers ws

--- polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_computers.py
+++ polklibrary.webservices/src/polklibrary/webservices/browser/ws_get_computers.py
@@ -35,18 +35,48 @@ class WSView(BrowserView):
         return data
         
     def call_keyserver(self, url):
+        url = url + '?nocache=' + str(int(time.time()))
         request = requests.get(url, verify=False, timeout=15)
         soup = BeautifulSoup(request.text)        
-        divs = soup.findAll('div', {'class': lambda x: x and 'av-comp' in x})
-
+        
         computers = {}
-        for div in divs:
-            if 'data-info-name' in str(div) and 'class' in str(div):
-                if 'used' in div['class']:
-                    computers[div['data-info-name']] = 0;
-                else:
-                    computers[div['data-info-name']] = 1;
+        elements = soup.findAll('div', {'class': lambda x: x and 'av-comp' in x})
+        if elements: # DIV FINDER
+            #print("DIV FINDER "  + str(len(elements)))
+            for elem in elements:
+                if 'data-info-name' in str(elem) and 'class' in str(elem):
+                    if 'used' in elem['class']:
+                        computers[elem['data-info-name']] = 0;
+                    else:
+                        computers[elem['data-info-name']] = 1;
+                        
+        else: # TR FINDER
+            elements = soup.findAll('tr', {'class': lambda x: x and 'av-comp' in x})
+            #print("TR FINDER " + str(len(elements)))
+            for elem in elements:
+                if 'data-info-id' in str(elem) and 'class' in str(elem):
+                    innerelem = elem.find('span', {'class':'av-name'})
+                    if 'used' in elem['class']:
+                        computers[innerelem['data-name']] = 0;
+                    else:
+                        computers[innerelem['data-name']] = 1;   
+                    
+                    
         return computers
+        
+    # def call_keyserver(self, url):
+        # request = requests.get(url, verify=False, timeout=15)
+        # soup = BeautifulSoup(request.text)        
+        # divs = soup.findAll('div', {'class': lambda x: x and 'av-comp' in x})
+
+        # computers = {}
+        # for div in divs:
+            # if 'data-info-name' in str(div) and 'class' in str(div):
+                # if 'used' in div['class']:
+                    # computers[div['data-info-name']] = 0;
+                # else:
+                    # computers[div['data-info-name']] = 1;
+        # return computers
 
         
         
